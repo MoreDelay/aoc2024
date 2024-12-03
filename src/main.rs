9b075d603c707b1cb12552c06610a07269454ec0
Dennis Ruppel <crap-dennis@gmx.net>
2024-12-03 12:24:05 +0100

    fix dynamic programming approach

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     fs::File,
     io::{BufRead, BufReader},
     iter::zip,
+    ops::BitAnd,
     path::{Path, PathBuf},
 };
 
@@ -123,57 +124,48 @@ fn brute_force(row: &[usize]) -> bool {
 fn validate_record_dampened(row: &[usize], skips: usize) -> bool {
     let skips = min(row.len() - 1, skips);
 
-    // dynamic program:
+    // dynamic program: resolve recursive evaluation from the back
     // mark if record valid using 0, 1, ... skips
-    // value indicates the index of last value to compare to
-    let mut increasing_table = vec![vec![None; row.len()]; skips + 1];
-    let mut decreasing_table = vec![vec![None; row.len()]; skips + 1];
+    let mut increasing_table = vec![vec![false; row.len()]; skips + 1];
+    let mut decreasing_table = vec![vec![false; row.len()]; skips + 1];
 
     let gradual = |a, b| (max(a, b) - min(a, b)) <= 3;
     let increasing = |a, b| (a < b) && gradual(a, b);
     let decreasing = |a, b| (a > b) && gradual(a, b);
 
-    // initialize: skipping all values until no longer able to is valid
-    for index in 0..=skips {
-        increasing_table[index][index] = Some(index);
-        decreasing_table[index][index] = Some(index);
-    }
-
-    for index in 1..row.len() {
-        let cur = row[index];
-
-        let no_skip_incr = increasing_table[0][index - 1]
-            .is_some_and(|last_index| increasing(row[last_index], cur))
-            .then_some(index);
-        let no_skip_decr = decreasing_table[0][index - 1]
-            .is_some_and(|last_index| decreasing(row[last_index], cur))
-            .then_some(index);
-
-        increasing_table[0][index] = no_skip_incr;
-        decreasing_table[0][index] = no_skip_decr;
-    }
-
-    for skip in 1..=skips {
-        for index in (skip + 1)..row.len() {
-            let cur = row[index];
-
-            let no_skip_incr = increasing_table[skip][index - 1]
-                .is_some_and(|last_index| increasing(row[last_index], cur))
-                .then_some(index);
-            let no_skip_decr = decreasing_table[skip][index - 1]
-                .is_some_and(|last_index| decreasing(row[last_index], cur))
-                .then_some(index);
-
-            let skip_here_incr = increasing_table[skip - 1][index - 1];
-            let skip_here_decr = decreasing_table[skip - 1][index - 1];
-
-            increasing_table[skip][index] = skip_here_incr.or(no_skip_incr);
-            decreasing_table[skip][index] = skip_here_decr.or(no_skip_decr);
+    // initialize: at the end you can skip to completion
+    for skip in 0..=skips {
+        for index in 0..=skip {
+            increasing_table[skip][row.len() - 1 - index] = true;
+            decreasing_table[skip][row.len() - 1 - index] = true;
         }
     }
 
-    increasing_table[skips][row.len() - 1].is_some()
-        || decreasing_table[skips][row.len() - 1].is_some()
+    for skip in 0..=skips {
+        for index in (0..row.len() - 1 - skip).rev() {
+            let cur = row[index];
+            for used_skips in 0..=skip {
+                let next_index = index + used_skips + 1;
+                let next = row[next_index];
+
+                let incr_valid =
+                    increasing_table[skip - used_skips][next_index].bitand(increasing(cur, next));
+                let decr_valid =
+                    decreasing_table[skip - used_skips][next_index].bitand(decreasing(cur, next));
+
+                increasing_table[skip][index] |= incr_valid;
+                decreasing_table[skip][index] |= decr_valid;
+            }
+        }
+
+        // first entry can be skipped entirely
+        if skip > 0 {
+            increasing_table[skip][0] |= increasing_table[skip - 1][1];
+            decreasing_table[skip][0] |= decreasing_table[skip - 1][1];
+        }
+    }
+
+    increasing_table[skips][0] || decreasing_table[skips][0]
 }
 
 fn day_02() -> Result<()> {
@@ -199,10 +191,21 @@ fn day_02() -> Result<()> {
         .count();
     println!("valid={valid_records}");
 
-    let dampened_records: Vec<_> = data.iter().filter(|&row| brute_force(row)).collect();
+    let brute_forced: Vec<_> = data.iter().filter(|&row| brute_force(row)).collect();
+    let dampened_records: Vec<_> = data
+        .iter()
+        .filter(|&row| validate_record_dampened(row, 1))
+        .collect();
     let dampened_valid = dampened_records.len();
 
     println!("dampened={dampened_valid}");
+
+    for r in brute_forced {
+        let found = dampened_records.iter().find(|&&e| e == r).is_some();
+        if !found {
+            println!("unfound: {r:?}");
+        }
+    }
     Ok(())
 }
 
@@ -288,7 +291,7 @@ mod tests {
 
     #[test]
     fn test_decr_skip_middle() {
-        let row = vec![62, 61, 60, 61, 59];
+        let row = vec![38, 34, 37, 35, 34];
         let valid = validate_record_dampened(&row, 1);
         assert!(valid);
     }
@@ -298,12 +301,5 @@ mod tests {
         let row = vec![62, 62, 60, 60, 59];
         let valid = validate_record_dampened(&row, 1);
         assert!(!valid);
-    }
-
-    #[test]
-    fn test_example_1() {
-        let row = vec![7, 6, 4, 2, 1];
-        let valid = validate_record_dampened(&row, 1);
-        assert!(valid);
     }
 }
