0481bd7a30009b9dd5373a1c934ef2bb44a0e0f8
Dennis Ruppel <crap-dennis@gmx.net>
2024-12-11 01:34:08 +0100

    day 07 part 2

--- src/day07.rs
+++ src/day07.rs
@@ -34,6 +34,7 @@ fn generate_equations(input: &str) -> Result<Vec<Equation>> {
 enum NextOperators {
     Mul,
     Add,
+    Concat,
     Done,
 }
 
@@ -96,6 +97,79 @@ fn can_construct_equation(equation: &Equation) -> bool {
                 });
             }
             NextOperators::Done => (),
+            NextOperators::Concat => unreachable!(),
+        };
+    }
+    false
+}
+// Depth First Search with backtracking when result gets too large
+fn can_construct_equation_with_concat(equation: &Equation) -> bool {
+    let Equation { target, values } = equation;
+    let mut stack = Vec::new();
+    let initial_state = State {
+        index: 0,
+        result: equation.values[0],
+        next_op: NextOperators::Mul,
+    };
+    stack.push(initial_state);
+    while !stack.is_empty() {
+        let State {
+            index,
+            result,
+            next_op,
+        } = stack.pop().expect("stack not empty");
+        let next_index = index + 1;
+        if next_index == values.len() {
+            if result == *target {
+                return true;
+            } else {
+                continue;
+            }
+        }
+        let next_value = values[next_index];
+        match next_op {
+            NextOperators::Mul => {
+                let next_result = result * next_value;
+                stack.push(State {
+                    index,
+                    result,
+                    next_op: NextOperators::Add,
+                });
+                stack.push(State {
+                    index: next_index,
+                    result: next_result,
+                    next_op: NextOperators::Mul,
+                });
+            }
+            NextOperators::Add => {
+                let next_result = result + next_value;
+                stack.push(State {
+                    index,
+                    result,
+                    next_op: NextOperators::Concat,
+                });
+                stack.push(State {
+                    index: next_index,
+                    result: next_result,
+                    next_op: NextOperators::Mul,
+                });
+            }
+            NextOperators::Concat => {
+                let mut concatted = result.to_string();
+                concatted.extend(next_value.to_string().chars());
+                let next_result: usize = concatted.parse().unwrap();
+                stack.push(State {
+                    index,
+                    result,
+                    next_op: NextOperators::Done,
+                });
+                stack.push(State {
+                    index: next_index,
+                    result: next_result,
+                    next_op: NextOperators::Mul,
+                });
+            }
+            NextOperators::Done => (),
         };
     }
     false
@@ -109,6 +183,14 @@ fn get_total_calibration_result(equations: &[Equation]) -> usize {
         .sum()
 }
 
+fn get_total_calibration_result_with_concat(equations: &[Equation]) -> usize {
+    equations
+        .iter()
+        .filter(|e| can_construct_equation_with_concat(e))
+        .map(|e| e.target)
+        .sum()
+}
+
 pub fn run() -> Result<()> {
     println!("day 07");
     let path = PathBuf::from("./resources/day07.txt");
@@ -116,5 +198,7 @@ pub fn run() -> Result<()> {
     let equations = generate_equations(&data)?;
     let result = get_total_calibration_result(&equations);
     println!("total calibration result: {result}");
+    let result = get_total_calibration_result_with_concat(&equations);
+    println!("total calibration result with concat: {result}");
     Ok(())
 }
