ac60fdbc3f1c58c37aa30f60c0fd3fd756e19cec
Dennis Ruppel <crap-dennis@gmx.net>
2024-12-22 05:28:54 +0100

    day 16 part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,17 +1,22 @@
 use anyhow::Result;
-use std::{fmt::Write, path::PathBuf};
+use std::{
+    collections::{HashSet, VecDeque},
+    fmt::Write,
+    marker::PhantomData,
+    path::PathBuf,
+};
 
 use crate::util::{self, AocError};
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 struct Pos(usize, usize);
 
 #[derive(Copy, Clone, Debug, Default)]
 struct Visited {
-    north: bool,
-    east: bool,
-    south: bool,
-    west: bool,
+    north: Option<usize>,
+    east: Option<usize>,
+    south: Option<usize>,
+    west: Option<usize>,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -23,10 +28,10 @@ enum Tile {
 }
 
 impl Tile {
-    fn visited(self, dir: Direction) -> bool {
+    fn visited(self, dir: Direction) -> Option<usize> {
         let visited = match self {
             Tile::Empty(visited) => visited,
-            Tile::Wall => return false,
+            Tile::Wall => return None,
             Tile::Start(visited) => visited,
             Tile::End(visited) => visited,
         };
@@ -38,7 +43,7 @@ impl Tile {
         }
     }
 
-    fn visit(&mut self, dir: Direction) {
+    fn visit(&mut self, dir: Direction, index: usize) {
         let visited = match self {
             Tile::Empty(visited) => visited,
             Tile::Wall => return,
@@ -46,10 +51,10 @@ impl Tile {
             Tile::End(visited) => visited,
         };
         match dir {
-            Direction::North => visited.north = true,
-            Direction::East => visited.east = true,
-            Direction::South => visited.south = true,
-            Direction::West => visited.west = true,
+            Direction::North => visited.north = Some(index),
+            Direction::East => visited.east = Some(index),
+            Direction::South => visited.south = Some(index),
+            Direction::West => visited.west = Some(index),
         };
     }
 }
@@ -68,7 +73,7 @@ impl PartialEq for Tile {
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 enum Direction {
     North,
     East,
@@ -107,6 +112,7 @@ impl std::fmt::Display for Tile {
     }
 }
 
+#[derive(Clone)]
 struct Maze {
     size: Pos,
     start: Pos,
@@ -164,23 +170,50 @@ impl Maze {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Debug)]
 struct MoveNode {
     pos: Pos,
     facing: Direction,
     points: usize,
-    #[allow(dead_code)]
-    last: Option<usize>,
+    last: Vec<usize>,
 }
 
-struct Dijkstra<'a> {
-    maze: &'a mut Maze,
+struct Init;
+struct Done;
+
+trait DijkstraState {}
+impl DijkstraState for Init {}
+impl DijkstraState for Done {}
+
+struct Dijkstra<T: DijkstraState> {
+    maze: Maze,
     nodes: Vec<MoveNode>,
     unvisited: Vec<usize>,
-    best: Option<MoveNode>,
+    best_points: Option<usize>,
+    best_paths: Vec<MoveNode>,
+    state: PhantomData<T>,
 }
 
-impl<'a> Dijkstra<'a> {
+impl Dijkstra<Init> {
+    fn new(maze: Maze) -> Self {
+        let initial = MoveNode {
+            pos: maze.start,
+            facing: Direction::East,
+            points: 0,
+            last: Vec::new(),
+        };
+        let nodes = vec![initial];
+        let unvisited = vec![0];
+        Self {
+            maze,
+            nodes,
+            unvisited,
+            best_points: None,
+            best_paths: Vec::new(),
+            state: PhantomData,
+        }
+    }
+
     fn get_next_unvisited(&mut self) -> Option<(usize, MoveNode)> {
         if self.unvisited.is_empty() {
             return None;
@@ -198,13 +231,13 @@ impl<'a> Dijkstra<'a> {
 
         smallest_index.map(|ii| {
             let index = self.unvisited.remove(ii);
-            let node = self.nodes[index];
+            let node = self.nodes[index].clone();
             (index, node)
         })
     }
 
-    fn search_goal(&mut self) -> Option<MoveNode> {
-        while let Some((index, node)) = self.get_next_unvisited() {
+    fn solve(mut self) -> Dijkstra<Done> {
+        while let Some((cur_index, node)) = self.get_next_unvisited() {
             let MoveNode {
                 pos,
                 facing,
@@ -212,48 +245,69 @@ impl<'a> Dijkstra<'a> {
                 ..
             } = node;
 
-            // look around (assume out of bounds is wall)
+            if self.best_points.is_some_and(|p| p < points) {
+                // all further paths will have worse score
+                break;
+            }
+
             let current_tile = self.maze.at(pos);
+            if let Tile::End(_) = current_tile {
+                assert!(self.best_points.is_none_or(|p| p == points));
+                self.best_points = Some(points);
+                self.best_paths.push(node);
+                continue;
+            }
+
+            // look around (assume out of bounds is wall)
             let left = self.maze.neighbor_pos(pos, facing.turn_left());
             let left = left.map(|p| self.maze.at(p)).unwrap_or(Tile::Wall);
             let right = self.maze.neighbor_pos(pos, facing.turn_right());
             let right = right.map(|p| self.maze.at(p)).unwrap_or(Tile::Wall);
 
-            if let Tile::End(_) = current_tile {
-                self.best = Some(node);
-                return Some(node);
-            }
-
             // add new node for turning left
-            let turn_left = facing.turn_left();
-            if left != Tile::Wall && !current_tile.visited(turn_left) {
-                self.maze.at_mut(pos).visit(turn_left);
+            let facing_left = facing.turn_left();
+            if left != Tile::Wall {
+                if let Some(prev_index) = current_tile.visited(facing_left) {
+                    let prev_node = &mut self.nodes[prev_index];
+                    if prev_node.points == points + 1000 {
+                        prev_node.last.push(cur_index);
+                    }
+                } else {
+                    let new_index = self.nodes.len();
+                    let new_node = MoveNode {
+                        pos,
+                        facing: facing_left,
+                        points: points + 1000,
+                        last: vec![cur_index],
+                    };
+                    self.unvisited.push(new_index);
+                    self.nodes.push(new_node);
 
-                let new_index = self.nodes.len();
-                let new_node = MoveNode {
-                    pos,
-                    facing: turn_left,
-                    points: points + 1000,
-                    last: Some(index),
-                };
-                self.unvisited.push(new_index);
-                self.nodes.push(new_node);
+                    self.maze.at_mut(pos).visit(facing_left, new_index);
+                }
             }
 
             // add new node for turning right
-            let turn_right = facing.turn_right();
-            if right != Tile::Wall && !current_tile.visited(turn_right) {
-                self.maze.at_mut(pos).visit(turn_right);
+            let facing_right = facing.turn_right();
+            if right != Tile::Wall {
+                if let Some(prev_index) = current_tile.visited(facing_right) {
+                    let prev_node = &mut self.nodes[prev_index];
+                    if prev_node.points == points + 1000 {
+                        prev_node.last.push(cur_index);
+                    }
+                } else {
+                    let new_index = self.nodes.len();
+                    let new_node = MoveNode {
+                        pos,
+                        facing: facing_right,
+                        points: points + 1000,
+                        last: vec![cur_index],
+                    };
+                    self.unvisited.push(new_index);
+                    self.nodes.push(new_node);
 
-                let new_index = self.nodes.len();
-                let new_node = MoveNode {
-                    pos,
-                    facing: turn_right,
-                    points: points + 1000,
-                    last: Some(index),
-                };
-                self.unvisited.push(new_index);
-                self.nodes.push(new_node);
+                    self.maze.at_mut(pos).visit(facing_right, new_index);
+                }
             }
 
             let Some(new_pos) = self.maze.neighbor_pos(pos, facing) else {
@@ -262,51 +316,106 @@ impl<'a> Dijkstra<'a> {
 
             // add new node for moving forward
             let tile_ahead = self.maze.at(new_pos);
-            if tile_ahead != Tile::Wall && !tile_ahead.visited(facing) {
-                self.maze.at_mut(pos).visit(facing);
+            if tile_ahead != Tile::Wall {
+                if let Some(prev_index) = tile_ahead.visited(facing) {
+                    let prev_node = &mut self.nodes[prev_index];
+                    if prev_node.points == points + 1 {
+                        prev_node.last.push(cur_index);
+                    }
+                } else {
+                    let new_index = self.nodes.len();
+                    let new_node = MoveNode {
+                        pos: new_pos,
+                        facing,
+                        points: points + 1,
+                        last: vec![cur_index],
+                    };
+                    self.unvisited.push(new_index);
+                    self.nodes.push(new_node);
 
-                let new_index = self.nodes.len();
-                let new_node = MoveNode {
-                    pos: new_pos,
-                    facing,
-                    points: points + 1,
-                    last: Some(index),
-                };
-                self.unvisited.push(new_index);
-                self.nodes.push(new_node);
+                    self.maze.at_mut(pos).visit(facing, new_index);
+                }
             }
         }
-        None
+
+        let Dijkstra {
+            maze,
+            nodes,
+            unvisited,
+            best_points,
+            best_paths,
+            ..
+        } = self;
+        Dijkstra {
+            maze,
+            nodes,
+            unvisited,
+            best_points,
+            best_paths,
+            state: PhantomData,
+        }
     }
 }
 
-fn solve_maze(maze: &mut Maze) -> Option<usize> {
-    let initial = MoveNode {
-        pos: maze.start,
-        facing: Direction::East,
-        points: 0,
-        last: None,
-    };
-    let nodes = vec![initial];
-    let unvisited = vec![0];
-    let mut dijkstra = Dijkstra {
-        maze,
-        nodes,
-        unvisited,
-        best: None,
-    };
+impl Dijkstra<Done> {
+    fn best_points(&self) -> Option<usize> {
+        self.best_points
+    }
 
-    let best = dijkstra.search_goal();
-    best.map(|n| n.points)
+    fn best_paths(&self) -> (&Vec<MoveNode>, &Vec<MoveNode>) {
+        (&self.best_paths, &self.nodes)
+    }
+}
+
+fn count_unique_pos_and_dir(
+    final_nodes: &[MoveNode],
+    nodes: &[MoveNode],
+) -> HashSet<(Pos, Direction)> {
+    let mut set = HashSet::new();
+    for final_node in final_nodes {
+        let MoveNode {
+            pos, facing, last, ..
+        } = final_node;
+        set.insert((*pos, *facing));
+        let mut queue: VecDeque<usize> = VecDeque::new();
+        queue.extend(last);
+        while let Some(last) = queue.pop_front() {
+            let node = &nodes[last];
+            let MoveNode {
+                pos, facing, last, ..
+            } = node;
+
+            if set.contains(&(*pos, *facing)) {
+                continue;
+            }
+            set.insert((*pos, *facing));
+            queue.extend(last);
+        }
+    }
+    set
+}
+
+fn count_seats(final_nodes: &[MoveNode], nodes: &[MoveNode]) -> usize {
+    let set = count_unique_pos_and_dir(final_nodes, nodes);
+    let set = set.iter().map(|&(p, _)| p).collect::<HashSet<_>>();
+    set.len()
 }
 
 pub fn run() -> Result<()> {
     println!("day 16");
     let path = PathBuf::from("./resources/day16.txt");
     let data = util::get_data_string(&path)?;
-    let mut maze = Maze::parse(&data)?;
-    let best = solve_maze(&mut maze).ok_or(AocError::ValueError("could not solve maze".into()))?;
-    println!("least points to solve maze: {best}");
+    let maze = Maze::parse(&data)?;
+    let dijkstra = Dijkstra::new(maze);
+    let dijkstra = dijkstra.solve();
+    let best_points = dijkstra
+        .best_points()
+        .ok_or(AocError::ValueError("could not solve maze".into()))?;
+    println!("least points to solve maze: {best_points}");
+
+    let (final_nodes, all_nodes) = dijkstra.best_paths();
+    let count = count_seats(final_nodes, all_nodes);
+    println!("number of seats: {count}");
     Ok(())
 }
 
@@ -331,8 +440,68 @@ mod tests {
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############";
-        let mut maze = Maze::parse(input).unwrap();
-        let best = solve_maze(&mut maze).unwrap();
-        assert_eq!(best, 7036);
+        let maze = Maze::parse(input).unwrap();
+        let dijkstra = Dijkstra::new(maze);
+        let dijkstra = dijkstra.solve();
+        let best_points = dijkstra.best_points().unwrap();
+        assert_eq!(best_points, 7036);
+    }
+
+    #[test]
+    fn test_maze_path_unique_pos_and_dir() {
+        let input = "###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############";
+        let maze = Maze::parse(input).unwrap();
+        let dijkstra = Dijkstra::new(maze);
+        let dijkstra = dijkstra.solve();
+        let (final_nodes, all_nodes) = dijkstra.best_paths();
+        let set = count_unique_pos_and_dir(final_nodes, all_nodes);
+        assert!(set.contains(&(Pos(1, 13), Direction::East)));
+        assert!(set.contains(&(Pos(1, 13), Direction::North)));
+        assert!(set.contains(&(Pos(1, 10), Direction::North)));
+        assert!(set.contains(&(Pos(2, 11), Direction::East)));
+        assert!(set.contains(&(Pos(3, 10), Direction::North)));
+        assert!(set.contains(&(Pos(3, 8), Direction::North)));
+        assert!(set.contains(&(Pos(4, 11), Direction::East)));
+        assert!(set.contains(&(Pos(6, 7), Direction::East)));
+    }
+
+    #[test]
+    fn test_maze_seats() {
+        let input = "###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############";
+        let maze = Maze::parse(input).unwrap();
+        let dijkstra = Dijkstra::new(maze);
+        let dijkstra = dijkstra.solve();
+        let (final_nodes, all_nodes) = dijkstra.best_paths();
+        let count = count_seats(final_nodes, all_nodes);
+
+        assert_eq!(count, 45);
     }
 }
